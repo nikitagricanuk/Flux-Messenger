340e77eb9dd2b78e59780009b0624bc2a6937b07
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2026-03-08 14:33:45 +0800

    Added DB

--- apps/backend/src/main/java/ru/flux/flux/FluxApplication.java
+++ apps/backend/src/main/java/ru/flux/flux/FluxApplication.java
@@ -2,15 +2,8 @@ package ru.flux.flux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class FluxApplication {
     public static void main(String[] args) {
         SpringApplication.run(FluxApplication.class, args);
