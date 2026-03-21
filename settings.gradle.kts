c2849661ebd0796b68f1c1aec046b39f7a21e019
neurolius <ewgensafonow@gmail.com>
2026-03-21 21:36:53 +0800

    Инициализировано андроид приложение

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "flux"
 
 include(":apps:backend")
 include(":shared:api-contract")
 include(":shared:core")
 include(":shared:client-sdk")
-
+include(":apps:android")
 include(":apps:desktop")
